f6560283a5aa46d9afe8d1e7ffbf68cf03c0ac01
Ryan Cahela <ryancahela@gmail.com>
2019-02-23 13:16:01 -0800

    added formatting functions

--- script.js
+++ script.js
@@ -1,8 +1,52 @@
 function submitHandler(event){
   event.preventDefault();
-  let searchVal= $('[name="statesearch"]').val();
-  console.log(searchVal);
+  let stateSearchVal = $('.form__state-input').val();
+  getParkData(stateSearchVal);
 }
+
 $('.search-form').on('submit', submitHandler);
 
+
+function getParkData(stateSearchVal) {
+    let endpoint = 'https://api.nps.gov/api/v1/parks';
+    let query = `${endpoint}?stateCode=${stateSearchVal}`;
+    console.log(query);
+    let apiKey = 'PgnZNBF1eFlwu12cj9iDVet20makkg9JMroA4RlY';
+    const options = {
+      headers: new Headers({
+        "X-Api-Key": apiKey,
+        "Accept": "application/json",
+        "Content-Type": 'application/json;charset=UTF-8',
+        "Access-Control-Allow-Origin": "*"
+      })
+    }
+
+    console.log(options);
+
+    fetch(query,options)
+    .then((response)=> response.json())
+    .then((responseJson) => console.log(responseJson));
+
+}
+
+"https://api.nps.gov/api/v1/parks?stateCode=ca"
+
+
+
+
+
+
+
+
 // 'X-Api-Key: PgnZNBF1eFlwu12cj9iDVet20makkg9JMroA4RlY' 'https://developer.nps.gov/api/v1/parks?parkCode=acad'
+// function createStateSearchObj(userValue) {
+//   return {
+//     searchType: 'stateCode',
+//     value: userValue
+//   }
+// }
+
+
+// function formatUrlSearch(searchObj) {
+//   return `${searchObj.searchType}=${searchObj.value}`;
+// }
