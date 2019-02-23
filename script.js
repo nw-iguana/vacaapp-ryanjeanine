0eee2ea12c8025dcdf48eb96b5a7b2df39063897
Jeanine Hoffman <jeanine.hoffman1970@gmail.com>
2019-02-23 13:52:36 -0800

    state fetch works

--- script.js
+++ script.js
@@ -8,28 +8,29 @@ $('.search-form').on('submit', submitHandler);
 
 
 function getParkData(stateSearchVal) {
+  let apiKey = 'PgnZNBF1eFlwu12cj9iDVet20makkg9JMroA4RlY';
     let endpoint = 'https://api.nps.gov/api/v1/parks';
-    let query = `${endpoint}?stateCode=${stateSearchVal}`;
+    let query = `${endpoint}?stateCode=${stateSearchVal}&api_key${apiKey}`;
     console.log(query);
-    let apiKey = 'PgnZNBF1eFlwu12cj9iDVet20makkg9JMroA4RlY';
-    const options = {
-      headers: new Headers({
-        "X-Api-Key": apiKey,
-        "Accept": "application/json",
-        "Content-Type": 'application/json;charset=UTF-8',
-        "Access-Control-Allow-Origin": "*"
-      })
+    // const options = {
+    //     mode: "cors",
+    //   headers: new Headers({
+    //     "X-Api-Key": apiKey
+        // "Accept": "application/jsonp",
+        // "Content-Type": 'application/jsonp;charset=UTF-8',
+        // "Access-Control-Allow-Origin": "*"
+      // })
+      
+      // console.log(options);
+      
+      fetch(query)
+      .then((response)=> response.json())
+      .then((responseJson) => console.log(responseJson));
+      
     }
+// }
 
-    console.log(options);
-
-    fetch(query,options)
-    .then((response)=> response.json())
-    .then((responseJson) => console.log(responseJson));
-
-}
-
-"https://api.nps.gov/api/v1/parks?stateCode=ca"
+// "https://api.nps.gov/api/v1/parks?stateCode=ca"
 
 
 
